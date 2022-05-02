9e0aa55ee3e3b406d4e2ebdf75fa0834d34b42c8
vengababu5 <pcvbabu@gmail.com>
2022-05-02 12:26:30 +0530

    chart changes

--- lendborrow/src/App.js
+++ lendborrow/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
@@ -8,10 +9,14 @@ import SignUp from './components/signup'
 import Dashboard from './components/dashboard'
 
 function App() {
+  const [css, setCss] = useState(false);
+  const getData = (val) => {
+    setCss(val);
+  }
   return (
     <Router>
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        {!css && < nav className="navbar navbar-expand-lg navbar-light fixed-top">
           <div className="container">
             <Link className="navbar-brand" to={'/sign-in'}>
               Lend & Borrow
@@ -31,20 +36,20 @@ function App() {
               </ul>
             </div>
           </div>
-        </nav>
+        </nav>}
 
-        <div className="outer">
-          <div className="inner">
+        <div className='outer'>
+          <div className={css ? "dashboard" : "inner"}>
             <Routes>
               <Route exact path="/" element={<Login />} />
               <Route path="/sign-in" element={<Login />} />
               <Route path="/sign-up" element={<SignUp />} />
-              <Route path='/dashboard' element={<Dashboard />} />
+              <Route path='/dashboard' element={<Dashboard getData={getData} />} />
             </Routes>
           </div>
         </div>
       </div>
-    </Router>
+    </Router >
   )
 }
 
--- lendborrow/src/components/dashboard.js
+++ lendborrow/src/components/dashboard.js
@@ -1,12 +1,38 @@
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav'
+// import { MDBContainer } from "mdbreact";
+// import { CDBContainer } from 'cdbreact';
+// import { Bar } from 'react-chartjs-2';
 
-const Dashboard = () => {
+
+const Dashboard = ({ getData }) => {
     const tabs = [{ name: 'Home', value: 'first' }, { name: 'Groups', value: 'second' }, { name: 'Settings', value: 'third' }, { name: 'Invite', value: 'fourth' }, { name: 'Logout', value: 'fifth' }]
     const userInfo = [{ name: 'Total Balance', val: '$182.00' },
     { name: 'You Owe', val: '$102.61' }, { name: 'You are owed', val: '$284.61' },]
+    useEffect(() => {
+        getData(true);
+    }, []);
+    const [data] = useState({
+        labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+        datasets: [
+            {
+                label: 'My First dataset',
+                backgroundColor: 'rgba(194, 116, 161, 0.5)',
+                borderColor: 'rgb(194, 116, 161)',
+                data: [65, 59, 90, 81, 56, 55, 40],
+            },
+            {
+                label: 'My Second dataset',
+                backgroundColor: 'rgba(71, 225, 167, 0.5)',
+                borderColor: 'rgb(71, 225, 167)',
+                data: [28, 48, 40, 19, 96, 27, 100],
+            },
+        ],
+    });
+
     return (
         <Container>
             {/* Stack the columns on mobile by making one full-width and the other half-width */}
@@ -25,25 +51,39 @@ const Dashboard = () => {
 
 
                 </Col>
-                <Col xs={6} md={8}>
-                    <button type="submit" className="btn btn-color">ADD EXPENSES</button>
-                    <button type="submit" className="btn btn-color">SETTLE UP</button>
+                <Col xs={6} md={6}>
+
                     <Row>
+                        <Col md={5}><button type="submit" className="btn btn-color">ADD EXPENSES</button></Col>
+                        <Col md={4}><button type="submit" className="btn btn-color">SETTLE UP</button></Col>
+                    </Row>
+                    <Row className="mt-3">
                         {userInfo.map(val => {
                             return (<>
-                                <Col md={3}>
-                                    <div>
-                                        {val.name}
+                                <Col md={4}>
+                                    <div className='dashboard-color'>
+                                        <div>
+                                            {val.name}
 
+                                        </div>
+                                        <div>{val.val}</div>
                                     </div>
-                                    <div>{val.val}</div>
 
                                 </Col>
                             </>)
                         })}
 
                     </Row>
+                    <Row></Row>
                 </Col>
+
+                <Col md={4}>
+                    {/* <CDBContainer>
+                        <h3 className="mt-5">Bar chart</h3>
+                        <Bar data={data} options={{ responsive: true }} />
+                    </CDBContainer> */}
+                </Col>
+
             </Row>
 
         </Container>
